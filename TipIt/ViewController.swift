512cdd5c0ee0312f59d11c834ec0ba7ec8511887
ashish <avsavani@gmail.com>
2020-01-29 13:24:02 -0800

    Initial Commit

---
+++ TipIt/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  TipIt
+//
+//  Created by ashish on 1/29/20.
+//  Copyright © 2020 AshishSavani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentages = [0.15,0.2,0.25,0.1]
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+}
+
